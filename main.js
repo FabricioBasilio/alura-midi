b8504c932b62b26d0350e6348e1ccb4f99880357
Fabricio Basilio <fabriciobasilioo@gmail.com>
2024-09-21 16:00:06 -0300

    Adicionando duas funções de som

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-alert("teste")
+function tocarSomPom() {
+    document.querySelector("#som_tecla_pom").play();
+}
+
+function tocarSomClap() {
+    document.querySelector("#som_tecla_clap").play();
+}
+
+document.querySelector(".tecla_pom").addEventListener("click", tocarSomPom);
+
+document.querySelector(".tecla_clap").addEventListener("click", tocarSomClap);
+
