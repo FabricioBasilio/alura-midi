26872282f18cf51a4fdbd267fd5d1db20d3fdd4b
Fabricio Basilio <fabriciobasilioo@gmail.com>
2024-09-21 22:54:20 -0300

    Configurando o funcionamento dos botões

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-function tocarSomPom() {
-    document.querySelector("#som_tecla_pom").play();
+function tocarSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 let teclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
 
-while (contador < 9) {
-    teclas[contador].addEventListener("click", tocarSomPom);
+while (contador < teclas.length) {
+
+    const tecla = teclas[contador];
+
+    const instrumento = tecla.classList[1];
+    
+    const idAudio = `#som_${instrumento}`;
+    console.log(idAudio);
+    
+
+    teclas[contador].addEventListener("click", function() {
+        tocarSom(idAudio);
+    });
     contador++;
 }
