07468b2cf9429efac68330de713cef9b3fcb6aee
Fabricio Basilio <fabriciobasilioo@gmail.com>
2024-09-22 00:58:34 -0300

    Configurando as teclas do teclado

--- main.js
+++ main.js
@@ -10,16 +10,21 @@ for (let contador = 0; contador < teclas.length; contador++) {
 
     const instrumento = tecla.classList[1];
     
-    const idAudio = `#som_${instrumento}`;
-    console.log(idAudio);
-    
+    const idAudio = `#som_${instrumento}`; 
 
     tecla.addEventListener("click", function() {
         tocarSom(idAudio);
     });
 
-    tecla.addEventListener("keydown", function() {
-        // tocarSom(idAudio);   
+    tecla.addEventListener("keydown", function(evento) {
+        if (evento.code === "Space" || evento.code === "Enter") {
         tecla.classList.add("ativa");
+        }
     });
+
+    tecla.addEventListener("keyup", function() {
+        tecla.classList.remove("ativa");
+    });
+
+    
 }
